34d88e856a83df2e50ee4f9c12b992760a52a687
Adriano Castro <adrianostanley@gmail.com>
2024-04-11 15:23:15 -0300

    Add the support Files methods

--- src/Support/Files.js
+++ src/Support/Files.js
@@ -31,6 +31,57 @@ class Files {
     delete(file) {
         this.fs.unlinkSync(file);
     }
+
+    /**
+     * Lists the contents of a directory.
+     * 
+     * This is just a facade for fs.readdirSync().
+     * 
+     * @param {string} directory 
+     * @returns {string[]}
+     */
+    list(directory) {
+        return this.fs.readdirSync(directory);
+    }
+
+    /**
+     * Lists the screenshots in a directory.
+     * 
+     * To be a screenshot, the file extension must be .jpg, .jpeg, or .tga.
+     * 
+     * @param {string} directory 
+     * @returns {string[]}
+     */
+    listScreenshots(directory) {
+        return this.list(directory).filter(file => {
+            return file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.tga');
+        });
+    }
+
+    /**
+     * Moves a file from source to destination.
+     * 
+     * This is just a facade for fs.renameSync().
+     * 
+     * @param {string} source 
+     * @param {string} destination 
+     */
+    move(source, destination) {
+        this.fs.renameSync(source, destination);
+    }
+
+    /**
+     * Gets the application's base path.
+     * 
+     * If the path is provided, it will be appended to the base path.
+     * 
+     * @param {string} path
+     * 
+     * @returns {string}
+     */
+    path(path) {
+        return __dirname + '/../' + (path || '');
+    }
 }
 
 module.exports = Files;

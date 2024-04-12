69d31174d09f55466fa9aa6f2619fab7cc0a767c
Adriano Castro <adrianostanley@gmail.com>
2024-04-12 13:35:04 -0300

    Implement the Screenshot constructor

--- src/Models/Screenshot.js
+++ src/Models/Screenshot.js
@@ -3,12 +3,18 @@
  * 
  * This model contains all the information needed to work with a screenshot
  * file as well as the methods to manipulate it.
+ * 
+ * A screenshot is not tied to any World of Warcraft version, so it can be
+ * used for Classic, Retail, or any other version.
  */
 class Screenshot {
     /**
      * Creates a new Screenshot instance.
+     * 
+     * @param {string} path The path to the screenshot file.
      */
-    constructor() {
+    constructor(path) {
+        this.path = path;
     }
 }
 
--- tests/Models/Screenshot.test.js
+++ tests/Models/Screenshot.test.js
@@ -2,6 +2,8 @@ const Screenshot = require('../../src/Models/Screenshot');
 
 // @covers Screenshot
 test('Screenshot class is accessible and can be instantiated', () => {
-    const screenshot = new Screenshot();
+    const screenshot = new Screenshot('test-path');
+
     expect(screenshot).toBeInstanceOf(Screenshot);
+    expect(screenshot.path).toBe('test-path');
 });

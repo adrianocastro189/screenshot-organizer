6a5349782f878470048931647787bae0e0bac6ea
Adriano Castro <adrianostanley@gmail.com>
2024-04-15 10:37:23 -0300

    Reduce Screenshot tests to simpler callback functions

--- tests/Models/Screenshot.test.js
+++ tests/Models/Screenshot.test.js
@@ -4,94 +4,44 @@ const Screenshot = require('../../src/Models/Screenshot');
 const SAMPLE_SCREENSHOT_JPG_PATH = 'C:/Program Files (x86)/World of Warcraft/_classic_/Screenshots/WoWScrnShot_030123_150920.jpg';
 const SAMPLE_SCREENSHOT_TGA_PATH = 'C:/Program Files (x86)/World of Warcraft/_classic_/Screenshots/WoWScrnShot_030123_150920.tga';
 
-// @covers Screenshot
-test('Screenshot class is accessible and can be instantiated', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
 
+test('Screenshot.construct()', () => {
     expect(screenshot).toBeInstanceOf(Screenshot);
     expect(screenshot.path).toBe(SAMPLE_SCREENSHOT_JPG_PATH);
 });
 
-// @covers Screenshot.copy()
-test('Screenshot.copy() can copy the screenshot to the destination folder', () => {
+test('Screenshot.copy()', () => {
     // @TODO: Implement this method in CM2 <2024.04.15>
 });
 
-// @covers Screenshot.getDate()
-test('Screenshot.getDate() can return the date and time the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+test('Screenshot.getDate()', () => expect(screenshot.getDate()).toEqual(new Date('2023-03-01T15:09:20')));
 
-    expect(screenshot.getDate()).toEqual(new Date('2023-03-01T15:09:20'));
-});
+test('Screenshot.getDay()', () => expect(screenshot.getDay()).toBe(1));
 
-// @covers Screenshot.getDay()
-test('Screenshot.getDay() can return the day of the month the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+test('Screenshot.getDestination()', () => expect(screenshot.getDestination()).toBe('2023/03'));
 
-    expect(screenshot.getDay()).toBe(1);
-});
+test('Screenshot.getFilename()', () => expect(screenshot.getFilename()).toBe('WoWScrnShot_030123_150920.jpg'));
 
-// @covers Screenshot.getDestination()
-test('Screenshot.getDestination() can return the destination folder of the screenshot', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+test('Screenshot.getHour()', () => expect(screenshot.getHour()).toBe(15));
 
-    expect(screenshot.getDestination()).toBe('2023/03');
-});
+test('Screenshot.getMinute()', () => expect(screenshot.getMinute()).toBe(9));
 
-// @covers Screenshot.getFilename()
-test('Screenshot.getFilename() can extract the file name from the path', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+test('Screenshot.getMonth()', () => expect(screenshot.getMonth()).toBe(3));
 
-    expect(screenshot.getFilename()).toBe('WoWScrnShot_030123_150920.jpg');
-});
+test('Screenshot.getSecond()', () => expect(screenshot.getSecond()).toBe(20));
 
-// @covers Screenshot.getHour()
-test('Screenshot.getHour() can return the hour the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
+test('Screenshot.getYear()', () => expect(screenshot.getYear()).toBe(2023));
 
-    expect(screenshot.getHour()).toBe(15);
-});
-
-// @covers Screenshot.getMinute()
-test('Screenshot.getMinute() can return the minute the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
-
-    expect(screenshot.getMinute()).toBe(9);
-});
-
-// @covers Screenshot.getMonth()
-test('Screenshot.getMonth() can return the month the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
-
-    expect(screenshot.getMonth()).toBe(3);
-});
-
-// @covers Screenshot.getSecond()
-test('Screenshot.getSecond() can return the second the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
-
-    expect(screenshot.getSecond()).toBe(20);
-});
-
-// @covers Screenshot.getYear()
-test('Screenshot.getYear() can return the year the screenshot was taken', () => {
-    const screenshot = new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH);
-
-    expect(screenshot.getYear()).toBe(2023);
-});
-
-// @covers Screenshot.move()
-test('Screenshot.move() can move the screenshot to the destination folder', () => {
+test('Screenshot.move()', () => {
     // @TODO: Implement this method in CM2 <2024.04.15>
 });
 
-// @covers Screenshot.organize()
-test('Screenshot.organize() can organize the screenshot', () => {
+test('Screenshot.organize()', () => {
     // @TODO: Implement this method in FE2 <2024.04.15>
 });
 
-// @covers Screenshot.parseDate()
-test('Screenshot.parseDate() can extract the date and time from the file name', () => {
+test('Screenshot.parseDate()', () => {
     expect(new Screenshot(SAMPLE_SCREENSHOT_JPG_PATH).screenshotDate).toEqual(new Date('2023-03-01T15:09:20'));
     expect(new Screenshot(SAMPLE_SCREENSHOT_TGA_PATH).screenshotDate).toEqual(new Date('2023-03-01T15:09:20'));
 });

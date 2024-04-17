245707e0d23bbc8b1722313878826f14f4bf49f9
Adriano Castro <adrianostanley@gmail.com>
2024-04-17 11:09:06 -0300

    Add the Configuration.loadConfiguration() method

--- src/Models/Configuration.js
+++ src/Models/Configuration.js
@@ -8,6 +8,54 @@ class Configuration {
     constructor() {
         this.clients = [];
     }
+
+    /**
+     * Asserts that the configuration file exists.
+     * 
+     * If the configuration file doesn't exist, an error is thrown.
+     */
+    assertConfigurationFileExists() {
+        const configurationPath = this.getConfigurationPath();
+
+        if (!this.getFilesInstance().exists(configurationPath)) {
+            throw new Error('No config.json found!');
+        }
+    }
+
+    /**
+     * Gets the absolute path to the configuration file.
+     * 
+     * @returns {string} The path to the configuration file.
+     */
+    getConfigurationPath() {
+        return this.getFilesInstance().path('config.json');
+    }
+
+    /**
+     * Support method to get the Files instance.
+     * 
+     * @TODO: Update this method when app() is implemented and get its Files instance <2024.04.12>
+     */
+    getFilesInstance() {
+        const Files = require('../../src/Support/Files');
+        return new Files();
+    }
+
+    /**
+     * Loads the config.json file and sets the configuration properties.
+     * 
+     * The config.json file should be in the root of the project and can be
+     * copied from the config-example.json file.
+     */
+    loadConfiguration() {
+        this.assertConfigurationFileExists();
+
+        const configurationPath = this.getConfigurationPath();
+
+        this.properties = require(configurationPath);
+
+        return this;
+    }
 }
 
 module.exports = Configuration;
--- tests/Models/Configuration.test.js
+++ tests/Models/Configuration.test.js
@@ -1,8 +1,89 @@
 const Configuration = require('../../src/Models/Configuration');
+const Files = require('../../src/Support/Files');
 
 const configuration = new Configuration();
 
 test('Client.construct()', () => {
     expect(configuration).toBeInstanceOf(Configuration);
     expect(configuration.clients).toBeInstanceOf(Array);
+});
+
+test("Client.assertConfigurationFileExists() when config.json doesn't exist", () => {
+    const configuration = new Configuration();
+
+    // mocks the Files method to return false
+    configuration.getFilesInstance = () => {
+        return {
+            exists: () => {
+                return false;
+            },
+            path: (file) => {
+                return `/${file}`;
+            },
+        };
+    };
+
+    expect(() => {
+        configuration.assertConfigurationFileExists();
+    }).toThrow('No config.json found!');
+});
+
+test("Client.assertConfigurationFileExists() when config.json exists", () => {
+    const configuration = new Configuration();
+
+    // mocks the Files method to return false
+    configuration.getFilesInstance = () => {
+        return {
+            exists: () => {
+                return true;
+            },
+            path: (file) => {
+                return `/${file}`;
+            },
+        };
+    };
+
+    expect(() => {
+        configuration.assertConfigurationFileExists();
+    }).not.toThrow();
+});
+
+test('Client.getConfigurationPath()', () => {
+    const configuration = new Configuration();
+
+    // mocks the Files method to return a fake app path
+    configuration.getFilesInstance = () => {
+        return {
+            path: (file) => {
+                return `/${file}`;
+            }
+        };
+    };
+
+    expect(configuration.getConfigurationPath()).toBe('/config.json');
+});
+
+test('Client.loadConfiguration()', () => {
+    const configuration = new Configuration();
+
+    configuration.getConfigurationPath = () => {
+        return (new Files()).path('tests/Mocks/config.json');
+    }
+
+    configuration.loadConfiguration();
+
+    expect(configuration.properties).toEqual({
+        "clients": [
+            {
+                "path": "C:/Program Files (x86)/World of Warcraft/_classic_"
+            },
+            {
+                "path": "C:/Program Files (x86)/World of Warcraft/_retail_"
+            }
+        ],
+        "settings": {
+            "destinationFolder": "C:/Pictures/World of Warcraft Screenshots",
+            "syncMethod": "copy"
+        }
+    });
 });
